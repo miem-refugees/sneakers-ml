358073a046a225919fc2b8784c6f76008c31a375
searayeah <insslava@gmail.com>
2023-11-13 17:42:31 +0300

    12439->5822

--- src/data/merger.py
+++ src/data/merger.py
@@ -1,6 +1,5 @@
 import os
 import re
-from abc import ABC
 from pathlib import Path
 from string import ascii_letters, digits
 from typing import Union
@@ -11,9 +10,11 @@ import pandas as pd
 # color_words = set([word.strip().lower() for word in open(color_path, "r").readlines()])
 
 allowed_symbols = ascii_letters + digits + " "
-not_allowed_extra_symbols = ['‘', '’', '“', '”', '™', '®', "'", '"', "~"]
-allowed_extra_symbols = {'#', '&', '*', '+', ',', '-', '.', ':', '?', '_', 'ß', 'ç', 'é', 'ê', 'ü', 'β', "%", "!", "+",
-                         "=", '(', ')'}
+
+default_replacements = {"ê": "e", "ä": "a", "é": "e", "ç": "c", "ô": "o", "ü": "u", "&amp;": "&", "β": "beta",
+                        "ß": "beta", "–": "-", '‘': "'", '’': "'", '”': '"', '“': '"', "\'\'": '"'}
+
+whitespace_replacements = dict.fromkeys({"-", "_", "\\", "/", "&"}, " ")
 
 
 class Merger:
@@ -39,8 +40,8 @@ class Merger:
                 "highsnobiety": self.format_highsnobiety(self.datasets["highsnobiety"]),
                 "kickscrew": self.format_kickscrew(self.datasets["kickscrew"])}
 
-    @staticmethod
-    def format_superkicks(df):
+    def format_superkicks(self, df):
+        df["website"] = "superkicks"
         df.columns = [x.lower() for x in df.columns]
 
         df = df.drop(["product-dimensions", "collection_url", "generic-name", "weight", "imported-by", "manufacturer",
@@ -50,14 +51,16 @@ class Merger:
         df["pricecurrency"] = "INR"
         df["price"] = df["price"].apply(lambda x: float(x.replace("₹", "").replace(",", "")))
 
-        df["title_clean"] = df["title"]
+        df[["title_without_color", "color"]] = df["title"].apply(lambda x: pd.Series(self.split_title_and_color(x)))
 
-        # self.df = self.df.drop("description", axis=1, errors="ignore")  # мб пригодится потом
+        df["title_merge"] = df["title_without_color"].apply(self.format_merge_title)
+
+        df = df.drop("description", axis=1)
+
         return df
 
-    @staticmethod
-    def format_sneakerbaas(df):
-
+    def format_sneakerbaas(self, df):
+        df["website"] = "sneakerbaas"
         df.columns = [x.lower() for x in df.columns]
 
         df = df.drop("collection_url", axis=1)
@@ -72,14 +75,25 @@ class Merger:
                     return list(dict.fromkeys(colors))
 
         df["description_color"] = df["description"].apply(get_color)
+
+        df[["title_without_color", "color"]] = df["title"].apply(lambda x: pd.Series(self.split_title_and_color(x)))
+
+        df["title_merge"] = df["title_without_color"].apply(self.format_merge_title)
+
+        df = df.drop("description", axis=1)
+
         return df
 
-    @staticmethod
-    def format_footshop(df):
+    def format_footshop(self, df):
+        df["website"] = "footshop"
+        df.columns = [x.lower() for x in df.columns]
+
         df = df.drop(["collection_url"], axis=1)
         df = df.drop_duplicates(subset=["title", "collection_name", "url"])
 
         df["price"] = df["price"].apply(lambda x: float(x.replace("€", "").replace("$", "")))
+
+        df["title_without_color"] = df["title"].apply(lambda x: self.apply_replacements(x, default_replacements))
 
         def get_color(text):
             colors = text.replace("&", "/").replace("/ ", "/").lower().split("/")
@@ -87,73 +101,102 @@ class Merger:
 
         df["color"] = df["color"].apply(get_color)
 
+        df["title_merge"] = df["title"].apply(self.format_merge_title)
+
         return df
 
-    @staticmethod
-    def format_kickscrew(df):
+    def format_kickscrew(self, df):
+        df["website"] = "kickscrew"
+        df.columns = [x.lower() for x in df.columns]
+
+        df = df.drop("slug", axis=1)
+
+        df["images_path"] = df.apply(
+            lambda columns: [columns["right-side-img"], columns["left-side-img"], columns["front-both-img"]], axis=1)
+        df = df.drop(["right-side-img", "left-side-img", "front-both-img"], axis=1)
+
+        df = df.drop_duplicates(subset=["title", "brand", "url"])
+
+        df[["title_without_color", "color"]] = df["title"].apply(lambda x: pd.Series(self.split_title_and_color(x)))
+
+        df["title_merge"] = df["title_without_color"].apply(self.format_merge_title)
+        return df
+
+    def format_highsnobiety(self, df):
+        df["website"] = "highsnobiety"
+        df.columns = [x.lower() for x in df.columns]
+
         df = df.drop("slug", axis=1)
         df["images_path"] = df.apply(
             lambda columns: [columns["right-side-img"], columns["left-side-img"], columns["front-both-img"]], axis=1)
         df = df.drop_duplicates(subset=["title", "brand", "url"])
         df = df.drop(["right-side-img", "left-side-img", "front-both-img"], axis=1)
-        return df
 
-    @staticmethod
-    def format_highsnobiety(df):
-        df = df.drop("slug", axis=1)
-        df["images_path"] = df.apply(
-            lambda columns: [columns["right-side-img"], columns["left-side-img"], columns["front-both-img"]], axis=1)
-        df = df.drop_duplicates(subset=["title", "brand", "url"])
-        df = df.drop(["right-side-img", "left-side-img", "front-both-img"], axis=1)
+        df["title_without_color"] = df["title"].apply(lambda x: self.apply_replacements(x, default_replacements))
+
+        # todo
+        df["title_merge"] = df["title_without_color"].apply(self.format_merge_title)
         return df
 
     @classmethod
-    def format_title(cls, text):
-        text = text.replace("/", " ")
-        text = text.replace("|", " ")
-        text = text.replace("–", "-")
-        text = text.replace("&amp;", "&")
+    def split_title_and_color(cls, title: str):
+        title = cls.apply_replacements(title, default_replacements)
+        title = cls.remove_double_quotes(title)
+        for quote in ["'", '"']:
+            end_quote = title.rfind(quote)
+            second_last_quote = title.rfind(quote, 0, end_quote - 1) if end_quote != -1 else -1
 
-        text = cls.remove_extra_symbols(text)
+            if second_last_quote != -1:
+                title_part = title[:second_last_quote].strip()
+                color_part = title[second_last_quote + 1:end_quote].strip()
+                return title_part, color_part
 
-        text = cls.remove_extra_whitespaces(text)
+        return title, ""
+
+    @staticmethod
+    def apply_replacements(text, replacements):
+        for key, value in replacements.items():
+            text = text.replace(key.lower(), value.lower())
+            text = text.replace(key.upper(), value.upper())
+        return text
+
+    @classmethod
+    def format_merge_title(cls, text):
         text = text.lower()
 
-        text = cls.remove_color_words(text)
+        text = cls.apply_replacements(text, default_replacements)
+        text = cls.apply_replacements(text, whitespace_replacements)
+        text = cls.remove_extra_symbols(text)
+        text = cls.remove_extra_whitespaces(text)
 
-        return text
+        return text.strip()
 
     @staticmethod
     def remove_extra_symbols(input_string: str) -> str:
-        # return ''.join(char for char in input_string if char not in not_allowed_extra_symbols)
         return ''.join(char for char in input_string if char in allowed_symbols).strip()
 
     @staticmethod
     def remove_extra_whitespaces(text):
-        if pd.notnull(text):
-            return " ".join(text.split())
-        else:
-            return text
-
-    # @staticmethod
-    # def remove_color_words(text):
-    #     # мб идти справа налево
-    #     out = []
-    #     text_split = text.split()
-    #     for word in text_split:
-    #         if word not in color_words:
-    #             out.append(word)
-    #     return " ".join(out)
+        return " ".join(text.split())
 
     @staticmethod
-    def check_extra_symbols(datasets: dict, column="title"):
-        def get_extra_symbols(df: pd.DataFrame, column="title") -> set:
-            out = set()
-            for text in df[column].tolist():
-                for symbol in text:
-                    if symbol not in allowed_symbols and symbol not in allowed_extra_symbols:
-                        out.add(symbol)
-            return out
+    def remove_double_quotes(text):
+        text = text.replace('""', '"')
+        text = text.replace("''", "'")
+        return text
 
-        for dataset in datasets:
-            print(dataset, get_extra_symbols(datasets[dataset], column))
+    @staticmethod
+    def get_extra_symbols(df: pd.DataFrame, column="title"):
+        out = dict()
+        for text in df[column].tolist():
+            for symbol in text:
+                if symbol not in allowed_symbols and symbol not in default_replacements and symbol not in whitespace_replacements:
+                    if symbol not in out:
+                        out[symbol] = []
+                    out[symbol].append(text)
+        return out
+
+    @classmethod
+    def check_extra_symbols(cls, datasets: dict, column="title"):
+        for dataset_name, dataset in datasets.items():
+            print(dataset_name, cls.get_extra_symbols(dataset, column).keys())

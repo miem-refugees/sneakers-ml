850a7a0dc85fcb4e2b1b1a1fb523f2e0509e1220
Daniil <ksusonic@yandex.ru>
2024-02-23 19:30:36 +0300

    restructured bot

---
+++ sneakers_ml/bot/controller/image.py
@@ -0,0 +1,39 @@
+import aiohttp
+from aiohttp import ClientResponseError, FormData
+from loguru import logger
+from telegram import Update
+from telegram.constants import ParseMode
+from telegram.ext import ContextTypes
+
+
+class ImageController:
+    def __init__(self, api_url: str) -> None:
+        self.api_url = api_url
+        self.classify_brand_url = f"{api_url}/classify-brand/upload/"
+
+    async def classify_brand(self, image: bytearray, username: str, filename: str) -> str:
+        try:
+            async with aiohttp.ClientSession() as session:
+                data = FormData()
+                data.add_field("image", image, filename=filename)
+                data.add_field("username", username)
+                resp = await session.post(self.classify_brand_url, data=data)
+                content = await resp.json()
+                resp.raise_for_status()
+                predictions = content
+                return "\n".join([f"*{model}*: *{predictions[model]}*" for model in predictions])
+        except ClientResponseError as e:
+            logger.error("Got response error from classify-brand: %s\n%s", e, content)
+        except Exception as e:
+            logger.error("Got exception while trying to classify brand: %s", e)
+
+        return "Oh no! Could not predict brand, sorry =("
+
+    async def image_handler(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        file_info = await context.bot.get_file(update.message.photo[-1].file_id)
+        image: bytearray = await file_info.download_as_bytearray()
+        await update.message.reply_text("Predicting brand for your photo...")
+
+        classification_result = await self.classify_brand(image, update.message.from_user.username, file_info.file_id)
+
+        await update.message.reply_text(classification_result, parse_mode=ParseMode.MARKDOWN_V2)
--- sneakers_ml/bot/main.py
+++ sneakers_ml/bot/main.py
@@ -1,59 +1,22 @@
-import logging
-
-import aiohttp
-from aiohttp.client_exceptions import ClientResponseError
-from aiohttp.formdata import FormData
+from loguru import logger
 from telegram import Update
-from telegram.constants import ParseMode
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
 
 from sneakers_ml.bot.config import Config
-
-logger = logging.getLogger()
-CLASSIFY_URL = "http://localhost:8000/classify-brand/upload/"
-
-
-async def start(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(
-        f"Hello, {update.message.from_user.full_name}!\n"
-        + "Just send me a photo of you sneaker and I'll try to predict it's brand"
-    )
-
-
-async def image(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    file_info = await context.bot.get_file(update.message.photo[-1].file_id)
-    image: bytearray = await file_info.download_as_bytearray()
-    await update.message.reply_text("Predicting brand for your photo...")
-    predictions = None
-
-    try:
-        async with aiohttp.ClientSession() as session:
-            data = FormData()
-            data.add_field("image", image, filename=file_info.file_id)
-            data.add_field("username", update.message.from_user.username)
-            resp = await session.post(CLASSIFY_URL, data=data)
-            content = await resp.json()
-            resp.raise_for_status()
-            predictions = content
-    except ClientResponseError as e:
-        logger.error("Got response error from classify-brand: %s\n%s", e, content)
-        await update.message.reply_text("Oh no! Could not predict brand, sorry =(")
-
-    if predictions is not None:
-        await update.message.reply_text(
-            "\n".join([f"*{model}*: *{predictions[model]}*" for model in predictions]), parse_mode=ParseMode.MARKDOWN_V2
-        )
-    else:
-        await update.message.reply_text("Oh no! Could not predict brand, sorry =(")
+from sneakers_ml.bot.controller.common import CommonController
+from sneakers_ml.bot.controller.image import ImageController
 
 
 def main():
     config = Config()
 
-    application = Application.builder().token(config.bot_token).build()
+    # Controllers
+    image_controller = ImageController(config.api_url)
+    common_controller = CommonController()
 
-    application.add_handler(CommandHandler(["start", "help"], start))
-    application.add_handler(MessageHandler(filters=filters.PHOTO, callback=image))
+    application = Application.builder().token(config.bot_token).build()
+    application.add_handler(CommandHandler(["start", "help"], common_controller.start_handler))
+    application.add_handler(MessageHandler(filters=filters.PHOTO, callback=image_controller.image_handler))
 
     logger.info("Started polling with %d handlers", len(application.handlers))
     application.run_polling(allowed_updates=Update.ALL_TYPES)

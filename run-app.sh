2f981569d15b5d9848d37ab912ee05fda9012ec6
Daniil <ksusonic@yandex.ru>
2024-03-24 22:43:37 +0000

    Better docker

--- run-app.sh
+++ run-app.sh
@@ -3,7 +3,8 @@
 set -e
 
 # Pull the DVC data
-dvc pull data/models/brands-classification.dvc -f
+echo "Pulling latest dvc data..."
+dvc pull data/models/brands-classification.dvc data/features/brands-classification.dvc -f
 
 # Start the FastAPI app with Uvicorn
 uvicorn sneakers_ml.app.main:app --proxy-headers --host 0.0.0.0 --port 8000

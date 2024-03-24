043884cb79b32315ec14415dc58ea77f7f5e8608
Daniil <ksusonic@yandex.ru>
2024-03-24 23:07:06 +0300

    Improvements (#95)

--- run-app.sh
+++ run-app.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -e
+
 # Pull the DVC data
-dvc pull data/models/brands-classification.dvc -f -d
+dvc pull data/models/brands-classification.dvc -f
 
 # Start the FastAPI app with Uvicorn
 uvicorn sneakers_ml.app.main:app --proxy-headers --host 0.0.0.0 --port 8000
